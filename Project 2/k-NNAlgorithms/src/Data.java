712f5df680ae0fccef4fb0c0f6dc3da48c88cd52
Rusty89 <rusty_clayton@yahoo.com>
2019-09-20 12:12:27 -0600

    Half of data is processed
    Functions for processing and bucketizing the data have been made.  File read in is done as well

--- Project 2/k-NNAlgorithms/src/Data.java
+++ Project 2/k-NNAlgorithms/src/Data.java
@@ -1,2 +1,265 @@
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Data {
+    ArrayList<ArrayList<ArrayList<Double>>> splitData= new ArrayList<ArrayList<ArrayList<Double>>>();
+    ArrayList<ArrayList<Double>> fullSet = new ArrayList<ArrayList<Double>>();
+    String [][] inputFileAsArray;
+    String dataName;
+
+    Data(File inputFileName, String dataName) throws Exception{
+        this.dataName=dataName;
+        this.inputFileAsArray=fileTo2dString(inputFileName);
+        preprocessData();
+        convertInputFileToArrayList();
+        bucketizeData();
+    }
+
+    ///////////////////////////////////////////////////////////////
+    //function for input data to be changed into a 2d string array
+    private  String [][] fileTo2dString(File inputFile) throws Exception{
+
+        //determines the dimensions of the file
+        int count=0;
+        Scanner sc = new Scanner(inputFile);
+        while (sc.hasNextLine()){
+            count++;
+            sc.nextLine();
+        }
+        sc.close();
+        sc = new Scanner(inputFile);
+        String [] line= sc.nextLine().split(",");
+
+        //once dimensions are found and 2d array data made, first line is added
+        String [][] data= new String [count][line.length];
+        //count is reset to increment the row while filling the array
+        count=0;
+        data[0]=line;
+        //rest of file is filled into the 2d array
+        while (sc.hasNextLine()){
+            count++;
+            data[count]= sc.nextLine().split(",");
+        }
+
+
+
+        if(dataName.equals("forestFire")){//resizes the forestFire file because it needs to have a row eliminated before rows
+            //are shuffled
+            String [][]temp2=new String[data.length-1][data[0].length];
+            for (int i = 1; i < data.length ; i++) {
+                for (int j = 0; j < data[0].length; j++) {
+                    temp2[i-1][j]=data[i][j];
+                }
+            }
+            data=temp2;
+        }
+        //randomize the order of the input array
+        //by swapping rows n times
+
+
+
+        for (int i = 0; i < data.length ; i++) {
+            //generate random number within range of data
+            int randomNum= (int)Math.floor((Math.random()*data.length));
+
+            for (int j = 0; j <data[0].length ; j++) {
+                //swaps data in random row and current row
+                String temp= data[i][j];
+                data[i][j]=data[randomNum][j];
+                data[randomNum][j]=temp;
+            }
+        }
+        sc.close();
+        return data;
+    }
+
+
+    private void preprocessData(){
+
+        //switch for data input
+        switch(dataName){
+
+            case "abalone"://makes sure abalone sex data is numerical
+                for (int i = 0; i <inputFileAsArray.length ; i++) {
+                    switch(inputFileAsArray[i][0]){
+                        case "F":
+                            inputFileAsArray[i][0]="0";
+                            break;
+                        case "M":
+                            inputFileAsArray[i][0]="1";
+                            break;
+                        case "I":
+                            inputFileAsArray[i][0]="2";
+                            break;
+                    }
+                }
+                break;
+
+            case "car"://changes values in car to numerical data
+                for (int i = 0; i <inputFileAsArray.length; i++) {
+                    for (int j = 0; j <inputFileAsArray[0].length ; j++) {
+                        switch(inputFileAsArray[i][j]){
+                            case "low":
+                                inputFileAsArray[i][j]="0";
+                                break;
+                            case "med":
+                                inputFileAsArray[i][j]="1";
+                                break;
+                            case "high":
+                                inputFileAsArray[i][j]="2";
+                                break;
+                            case "vhigh":
+                                inputFileAsArray[i][j]="3";
+                                break;
+                            case "small":
+                                inputFileAsArray[i][j]="0";
+                                break;
+                            case "big":
+                                inputFileAsArray[i][j]="2";
+                                break;
+                            case "more":
+                                inputFileAsArray[i][j]="5";
+                                break;
+                            case "5more":
+                                inputFileAsArray[i][j]="5";
+                                break;
+                            case "unacc":
+                                inputFileAsArray[i][j]="0";
+                                break;
+                            case "acc":
+                                inputFileAsArray[i][j]="1";
+                                break;
+                            case "good":
+                                inputFileAsArray[i][j]="2";
+                                break;
+                            case "vgood":
+                                inputFileAsArray[i][j]="3";
+                                break;
+                        }
+                    }
+                }
+                break;
+            case "forestFire":
+
+                //i starts at one to remove first row
+                for (int i = 0; i <inputFileAsArray.length; i++) {
+                    for (int j = 0; j < inputFileAsArray[0].length; j++) {
+                        switch (inputFileAsArray[i][j]) {
+                            case "jan":
+                                inputFileAsArray[i][j] = "0";
+                                break;
+                            case "feb":
+                                inputFileAsArray[i][j] = "1";
+                                break;
+                            case "mar":
+                                inputFileAsArray[i][j] = "2";
+                                break;
+                            case "apr":
+                                inputFileAsArray[i][j] = "3";
+                                break;
+                            case "may":
+                                inputFileAsArray[i][j] = "4";
+                                break;
+                            case "jun":
+                                inputFileAsArray[i][j] = "5";
+                                break;
+                            case "jul":
+                                inputFileAsArray[i][j] = "6";
+                                break;
+                            case "aug":
+                                inputFileAsArray[i][j] = "7";
+                                break;
+                            case "sep":
+                                inputFileAsArray[i][j] = "8";
+                                break;
+                            case "oct":
+                                inputFileAsArray[i][j] = "9";
+                                break;
+                            case "nov":
+                                inputFileAsArray[i][j] = "10";
+                                break;
+                            case "dec":
+                                inputFileAsArray[i][j] = "11";
+                                break;
+                            case "sat":
+                                inputFileAsArray[i][j] = "0";
+                                break;
+                            case "fri":
+                                inputFileAsArray[i][j] = "1";
+                                break;
+                            case "thu":
+                                inputFileAsArray[i][j] = "2";
+                                break;
+                            case "wed":
+                                inputFileAsArray[i][j] = "3";
+                                break;
+                            case "tue":
+                                inputFileAsArray[i][j] = "4";
+                                break;
+                            case "mon":
+                                inputFileAsArray[i][j] = "5";
+                                break;
+                            case "sun":
+                                inputFileAsArray[i][j] = "6";
+                                break;
+                        }
+                    }
+                }
+                inputFileAsArray=inputFileAsArray;
+                break;
+            case "machine":
+                break;
+            case "segmentation":
+                break;
+            case "wine":
+                break;
+        }
+
+    }
+
+    private void convertInputFileToArrayList(){
+        for (int i = 0; i <inputFileAsArray.length ; i++) {
+            ArrayList<Double> temp= new ArrayList<Double>();
+            for (int j = 0; j <inputFileAsArray[0].length; j++) {
+                temp.add(Double.parseDouble(inputFileAsArray[i][j]));
+            }
+            fullSet.add(temp);
+        }
+    }
+
+   private void bucketizeData()   {
+       int start1=0;
+       int start2;
+       for (int i = 0; i <10 ; i++) {
+           ArrayList<ArrayList<Double>> temp =new ArrayList<ArrayList<Double>>();
+           for (int j = 0; j <fullSet.size()*.9 ; j++) {//generates the training sets at size 90% of data
+               if(start1<fullSet.size()){
+                   temp.add(fullSet.get(start1));
+                   start1++;
+               }else{
+                   start1=0;
+                   temp.add(fullSet.get(start1));
+                   start1++;
+               }
+           }
+           start2=start1;//starts the testing set at last indice of training set + 1
+           splitData.add(temp);
+           temp =new ArrayList<ArrayList<Double>>();
+           for (int j = 0; j <fullSet.size()*.1 ; j++) {//generates the testing set of size 10% of data
+               if(start2<fullSet.size()){
+                   temp.add(fullSet.get(start2));
+                   start2++;
+               }else{
+                   start2=0;
+                   temp.add(fullSet.get(start2));
+               }
+           }
+           splitData.add(temp);
+       }
+
+
+   }
+
 }
--- Project 2/k-NNAlgorithms/src/Driver.java
+++ Project 2/k-NNAlgorithms/src/Driver.java
@@ -1,6 +1,26 @@
+import java.io.File;
+
 public class Driver {
 
-    public static void main(String args[]){
+    public static void main(String args[])throws Exception{
+
+        //for testing purposes
+        Data d1 = new Data(new File("../DataSets/forestfires.data"),"forestFire");
+        for (int k = 0; k <2 ; k++) {
+            if(k%2==0){
+                System.out.println("Training Set");
+            }else{
+                System.out.println("Testing Set");
+            }
+            for (int i = 0; i <d1.splitData.get(k).size() ; i++) {
+                for (int j = 0; j <d1.splitData.get(k).get(0).size() ; j++) {
+                    System.out.print(d1.splitData.get(k).get(i).get(j)+ " , ");
+                }
+                System.out.println();
+            }
+
+
+        }
 
     }
 }
