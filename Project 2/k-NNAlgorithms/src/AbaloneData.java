482d5122920a9ad379ca1cbc9bf9a9a98efed20b
Rusty Clayton <36992543+Rusty89@users.noreply.github.com>
2019-10-08 12:52:33 -0600

    Update AbaloneData.java
    updated some fixes in abalone

--- Project 2/k-NNAlgorithms/src/AbaloneData.java
+++ Project 2/k-NNAlgorithms/src/AbaloneData.java
@@ -18,18 +18,22 @@ public class AbaloneData extends Data {
         }
 
         for (int i = 0; i <fullSet.size() ; i++) {
-            Double classification = Double.parseDouble(fullSet.get(i).get(fullSet.get(0).size()-1));
-            //if num rings less than 10, set to class 0, young
-            if(classification<8){
-                fullSet.get(i).set(fullSet.get(0).size()-1,"0");
+            double classification = Double.parseDouble(fullSet.get(i).get(fullSet.get(0).size()-1));
+            double firstThird = 8;
+            double secondThird = 12;
+            int classIndex = fullSet.get(0).size()-1;
+            
+            // if num rings less than 8, set to class 0, young
+            if(classification<firstThird){
+                fullSet.get(i).set(classIndex,"0");
             }
-            //if num rings greater than or equal to 10 but less than 20, set to class 1, intermediate
-            else if(classification<12){
-                fullSet.get(i).set(fullSet.get(0).size()-1,"1");
+            // if num rings greater than or equal to 8 but less than 12, set to class 1, intermediate
+            else if(classification<secondThird){
+                fullSet.get(i).set(classIndex,"1");
             }
-            //if num rings greater than 20, set to class 2, old
+            // if num rings greater than 11, set to class 2, old
             else{
-                fullSet.get(i).set(fullSet.get(0).size()-1,"2");
+                fullSet.get(i).set(classIndex,"2");
             }
         }
     }
