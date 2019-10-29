1257b9c40ba339f5add355198deaa4c2192dfc23
GiantFrog <35314154+GiantFrog@users.noreply.github.com>
2019-10-29 15:39:29 -0600

    Add some functionality to Node, Network, and Layer

--- Project 3/NeuralNets/src/Layer.java
+++ Project 3/NeuralNets/src/Layer.java
@@ -2,12 +2,41 @@ import java.util.ArrayList;
 
 public class Layer
 {
+    private static final double minWeight = .05, weightRange = .095;
+    private Layer nextLayer, previousLayer;
     private ArrayList<Node> nodes;
 
     public Layer (int layerSize)
     {
         nodes = new ArrayList<>();
+        //TODO instead of each node having a random weight, should each connection between nodes have a random weight instead?
         for (int a = 0; a < layerSize; a++)
-            nodes.add(new Node(Math.random()*.095 + .05));
+            nodes.add(new Node(this, Math.random()*weightRange + minWeight));
+    }
+
+    //ensures none of the nodes in this layer will have weights above 1 or below 0.
+    public void normalizeNodes()
+    {
+        for (Node node: nodes)
+            node.normalize();
+    }
+
+    public ArrayList<Node> getNodes()
+    {
+        return nodes;
+    }
+    public Layer getNextLayer()
+    {
+        return nextLayer;
+    }
+    public Layer getPreviousLayer()
+    {
+        return previousLayer;
+    }
+    //also sets the previous layer's nextLayer as this.
+    public void setPreviousLayer (Layer layer)
+    {
+        previousLayer = layer;
+        layer.nextLayer = this;
     }
 }
--- Project 3/NeuralNets/src/Network.java
+++ Project 3/NeuralNets/src/Network.java
@@ -6,8 +6,19 @@ public class Network
 
     public Network (int[] layerSizes)
     {
+        //create a layer for each entry in layerSizes.
+        //each layer will be the size of the corresponding int in layerSizes.
         layers = new ArrayList<>();
         for (int size: layerSizes)
             layers.add(new Layer(size));
+
+        //inform each layer of its neighbors. setPreviousLayer() also updates the previous layer's nextLayer.
+        Layer previousLayer = null;
+        for (Layer layer: layers)
+        {
+            if (previousLayer != null)
+                layer.setPreviousLayer(previousLayer);
+            previousLayer = layer;
+        }
     }
 }
