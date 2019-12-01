9042f55e918e766530122ea46b55cb9b50a60462
Rusty89 <rusty_clayton@yahoo.com>
2019-12-01 09:39:06 -0700

    Deleted all nonessential stuff
    doesn't run yet

---
+++ Project 4/NeuralNets/src/FireData.java
@@ -0,0 +1,54 @@
+/* Inherits from Data class. Defines how we pre-process the Fire data set and holds the data from it
+ */
+
+import java.io.File;
+
+public class FireData extends Data {
+
+    // constructor that reads in, pre-processes, normalizes, and bucketizes (for cross-validation) a data set
+    FireData(File inputFileName) throws Exception {
+        numClasses = 5;
+        fileTo2dStringArrayList(inputFileName);
+        preProcess();
+        normalizeData();
+        bucketize();
+        findNumClassifications();
+    }
+
+    private void preProcess() {
+        for (int i = 0; i < fullSet.size(); i++) {
+            /*  removes the data representing the months
+                and days of the week as these will
+                not be valid data points using euclidean distance */
+            fullSet.get(i).remove(2);
+            fullSet.get(i).remove(2);
+        }
+
+        // normalize the classification value
+        double max = Double.MIN_VALUE;
+        double min = Double.MAX_VALUE;
+        int indexOfClass = fullSet.get(0).size()-1;
+        for (int i = 0; i < fullSet.size(); i++) {
+            double classValue = Double.parseDouble(fullSet.get(i).get(indexOfClass));
+            if( classValue >= max){
+                max = classValue;
+            }
+            if(classValue <= min){
+                min = classValue;
+            }
+        }
+
+        for (int i = 0; i < fullSet.size(); i++) {
+            double classValue = Double.parseDouble(fullSet.get(i).get(indexOfClass));
+            classValue = (classValue-min)/(max-min);
+            fullSet.get(i).set(indexOfClass, classValue+"");
+
+        }
+    }
+
+    // used to get the name of the dataset quickly for printing output
+    @Override
+    public String toString() {
+        return "fire";
+    }
+}
---
+++ Project 4/NeuralNets/src/Layer.java
@@ -0,0 +1,75 @@
+/* Layer class to be used in our MLP neural networks. A Network object is comprised of Layers. A Layer object is comprised
+    of Nodes. Layers are connected via a linked list in a network. This class includes methods for operating
+    on modes and getting/setting relevant values.
+ */
+
+import java.util.ArrayList;
+
+public class Layer
+{
+    private Layer nextLayer;
+    private Layer previousLayer;
+    private ArrayList<Node> nodes;
+
+    // layer constructor that adds nodes with given ranges to the layer
+    public Layer (int layerSize)
+    {
+        nodes = new ArrayList<>();
+
+        // adds nodes to a new layer when created
+        for (int a = 0; a < layerSize; a++)
+            nodes.add(new Node(this));
+    }
+
+    // getter /setter methods
+    public ArrayList<Node> getNodes()
+    {
+        return nodes;
+    }
+    public Node getNode (int index)
+    {
+        return nodes.get(index);
+    }
+    public Layer getNextLayer()
+    {
+        return nextLayer;
+    }
+    public Layer getPreviousLayer()
+    {
+        return previousLayer;
+    }
+
+
+    public void setPreviousLayer (Layer layer)
+    {
+        previousLayer = layer;
+        layer.nextLayer = this;
+    }
+
+    public void calculateOutput()
+    {
+        for (Node node: nodes)
+            node.calculateOutput();
+    }
+    public void initializeWeights (double minWeight, double weightRange)
+    {
+        for (Node node: nodes)
+            node.initializeWeights(minWeight, weightRange);
+    }
+
+    // returns index of highest valued node
+    public int getHighestValueNodeIndex()
+    {
+        double greatest = 0;
+        int greatestIndex = -1;
+        for (int a = 0; a < nodes.size(); a++)
+        {
+            if (nodes.get(a).output > greatest)
+            {
+                greatest = nodes.get(a).output;
+                greatestIndex = a;
+            }
+        }
+        return greatestIndex;
+    }
+}
