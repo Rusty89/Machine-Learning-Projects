8567584bd6b61e7790002b60222ef2de19fb6417
Rusty Clayton <36992543+Rusty89@users.noreply.github.com>
2019-12-07 16:23:23 -0700

    Revert "Ga tuning"

--- Project 4/NeuralNets/src/Driver.java
+++ Project 4/NeuralNets/src/Driver.java
@@ -10,14 +10,9 @@ import java.text.DecimalFormat;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
-    private static final int oneTenthNumGenerations = 10;
     private static final int numCondensedSets = 3;
     private static final int numFailuresAllowed = 30;
 
@@ -50,90 +45,58 @@ public class Driver {
         rData.add(redWine);
         rData.add(whiteWine);
 
-        runGeneticAlgorithmTests(rData, cData);
-
-        //runParticleSwarmTests(rData, cData, 30);
-
-        //runDifferentialEvoltionTests(rData, cData);
-    }
-
-    private static void printRangeAndMean (String name, ArrayList<Double> results)
-    {
-        DecimalFormat decimalFormat = new DecimalFormat("#.#########");
-        double sum = 0, min = Double.MAX_VALUE, max = Double.MIN_VALUE;
-        for (double result: results)
-        {
-            sum += result;
-            if (result < min)
-                min = result;
-            if (result > max)
-                max = result;
-        }
-        System.out.println(name + " from " + decimalFormat.format(min) + " to " + decimalFormat.format(max) +
-                " with a mean of " + decimalFormat.format(sum/results.size()) + ".");
-    }
-
-    private static void runGeneticAlgorithmTests (ArrayList<Data> rData, ArrayList<Data> cData)
-    {
-        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        Network[] best = new Network[3];
-
         // Train and test categorical data
         for (Data d : cData) {
             // For hidden layers 0, 1, and 2
             for (int i = 0; i < 3; i++) {
-                int finalI = i;  //save i as a new int for the runnable to use
-                executorService.execute(() -> {
-                    int[] layers = new int[2 + finalI];
-                    for (int j = 0; j < layers.length; j++) {
-                        if (j == layers.length - 1) {
-                            // Last layer is output layer
-                            layers[j] = d.numClasses;
-                        } else {
-                            // All layers except output are the same size, equal to the amount of features
-                            layers[j] = d.dataSets.trainingSets.get(0).get(0).size() - 1;
-                        }
+                int[] layers = new int[2 + i];
+                for (int j = 0; j < layers.length; j++) {
+                    if (j == layers.length - 1) {
+                        // Last layer is output layer
+                        layers[j] = d.numClasses;
+                    } else {
+                        // All layers except output are the same size, equal to the amount of features
+                        layers[j] = d.dataSets.trainingSets.get(0).get(0).size() - 1;
                     }
-                    // Initialize GA
-                    GeneticAlgorithm GA = new GeneticAlgorithm(d.dataSets.trainingSets.get(0), layers, d.numClasses, false);
-                    // Test on 10 generations for each training set - 100 generations total for training
-                    for (ArrayList<ArrayList<String>> trainingSet : d.dataSets.trainingSets) {
-                        // Update training set
-                        GA.trainSet = trainingSet;
-                        for (int k = 0; k < oneTenthNumGenerations; k++) {
-                            GA.runAlgorithm();
-                        }
-                    }
-                    // Grab best GA
-                    Network bestGA = GA.best;
 
-                    // Store results
-                    ArrayList<Double> GAprecision = new ArrayList<>();
-                    ArrayList<Double> GArecall = new ArrayList<>();
-                    ArrayList<Double> GAaccuracy = new ArrayList<>();
-                    // Test on all test sets
-                    for (ArrayList<ArrayList<String>> testSet : d.dataSets.testSets) {
-                        // On each example within the set
-                        for (ArrayList<String> test : testSet) {
-                            bestGA.initializeInputLayer(test);
-                            bestGA.feedForward();
-                            bestGA.guessHistory.add(bestGA.getClassNumber() + "");
-                        }
-                        // Process results
-                        ArrayList<String> GAloss = MathFunction.processConfusionMatrix(bestGA.guessHistory, testSet);
-                        bestGA.guessHistory.clear();
-                        GAprecision.add(Double.parseDouble(GAloss.get(0)));
-                        GArecall.add(Double.parseDouble(GAloss.get(1)));
-                        GAaccuracy.add(Double.parseDouble(GAloss.get(2)));
+                }
+                // Initialize GA
+                GeneticAlgorithm GA = new GeneticAlgorithm(d.dataSets.trainingSets.get(0), layers, d.numClasses, false);
+                // Test on 10 generations for each training set - 100 generations total for training
+                for (ArrayList<ArrayList<String>> trainingSet : d.dataSets.trainingSets) {
+                    // Update training set
+                    GA.trainSet = trainingSet;
+                    for (int k = 0; k < 10; k++) {
+                        GA.runAlgorithm();
                     }
-                    // Print results
-                    System.out.println("Hidden Layers = " + finalI);
-                    printRangeAndMean(d.toString() + " data - GAPrecision: ", GAprecision);
-                    printRangeAndMean(d.toString() + " data -    GARecall: ", GArecall);
-                    printRangeAndMean(d.toString() + " data -  GAAccuracy: ", GAaccuracy);
+                }
+                // Grab best GA
+                Network bestGA = GA.best;
 
-                    best[finalI] = bestGA;
-                });
+                // Store results
+                ArrayList<Double> GAprecision = new ArrayList<>();
+                ArrayList<Double> GArecall = new ArrayList<>();
+                ArrayList<Double> GAaccuracy = new ArrayList<>();
+                // Test on all test sets
+                for (ArrayList<ArrayList<String>> testSet : d.dataSets.testSets) {
+                    // On each example within the set
+                    for (ArrayList<String> test : testSet) {
+                        bestGA.initializeInputLayer(test);
+                        bestGA.feedForward();
+                        bestGA.guessHistory.add(bestGA.getClassNumber() + "");
+                    }
+                    // Process results
+                    ArrayList<String> GAloss = MathFunction.processConfusionMatrix(bestGA.guessHistory, testSet);
+                    bestGA.guessHistory.clear();
+                    GAprecision.add(Double.parseDouble(GAloss.get(0)));
+                    GArecall.add(Double.parseDouble(GAloss.get(1)));
+                    GAaccuracy.add(Double.parseDouble(GAloss.get(2)));
+                }
+                // Print results
+                System.out.println("Hidden Layers = " + i);
+                printRangeAndMean(d.toString() + " data - GAPrecision: ", GAprecision);
+                printRangeAndMean(d.toString() + " data -    GARecall: ", GArecall);
+                printRangeAndMean(d.toString() + " data -  GAAccuracy: ", GAaccuracy);
             }
         }
 
@@ -141,62 +104,57 @@ public class Driver {
         for (Data d : rData) {
             // For hidden layers 0, 1, 2
             for (int i = 0; i < 3; i++) {
-                int finalI = i;
-                executorService.execute(() -> {
-                    int[] layers = new int[2 + finalI];
-                    for (int j = 0; j < layers.length; j++) {
-                        if (j == layers.length - 1) {
-                            // Only 1 output node for regression
-                            layers[j] = 1;
-                        } else {
-                            // All layers except output are the same size, equal to the number of features
-                            layers[j] = d.dataSets.trainingSets.get(0).get(0).size() - 1;
-                        }
+                int[] layers = new int[2 + i];
+                for (int j = 0; j < layers.length; j++) {
+                    if (j == layers.length - 1) {
+                        // Only 1 output node for regression
+                        layers[j] = 1;
+                    } else {
+                        // All layers except output are the same size, equal to the number of features
+                        layers[j] = d.dataSets.trainingSets.get(0).get(0).size() - 1;
                     }
-                    // Initialize GA
-                    GeneticAlgorithm GA = new GeneticAlgorithm(d.dataSets.trainingSets.get(0), layers, 1, true);
-                    // Test on 10 generations for each training set - 100 generations total
-                    for (ArrayList<ArrayList<String>> trainingSet : d.dataSets.trainingSets) {
-                        // Update training set
-                        GA.trainSet = trainingSet;
-                        for (int k = 0; k < oneTenthNumGenerations; k++) {
-                            GA.runAlgorithm();
-                        }
+                }
+                // Initialize GA
+                GeneticAlgorithm GA = new GeneticAlgorithm(d.dataSets.trainingSets.get(0), layers, 1, true);
+                // Test on 10 generations for each training set - 100 generations total
+                for (ArrayList<ArrayList<String>> trainingSet : d.dataSets.trainingSets) {
+                    // Update training set
+                    GA.trainSet = trainingSet;
+                    for (int k = 0; k < 5; k++) {
+                        GA.runAlgorithm();
                     }
-                    // Grab best GA
-                    Network bestGA = GA.best;
+                }
+                // Grab best GA
+                Network bestGA = GA.best;
 
-                    // Store results
-                    ArrayList<Double> aeResults = new ArrayList<>();
-                    ArrayList<Double> mseResults = new ArrayList<>();
-                    // Test on each test set
-                    for (ArrayList<ArrayList<String>> testSet : d.dataSets.testSets) {
-                        double absErr = 0;
-                        double meanErr = 0;
-                        // On each example in the test set
-                        for (ArrayList<String> test : testSet) {
-                            bestGA.initializeInputLayer(test);
-                            bestGA.feedForward();
-                            bestGA.calcErr();
-                            // Tally errors
-                            absErr += Math.abs(bestGA.error);
-                            meanErr += Math.pow(bestGA.error, 2);
-                        }
-                        // Calculate results
-                        absErr /= testSet.size();
-                        meanErr /= testSet.size();
-                        aeResults.add(absErr);
-                        mseResults.add(meanErr);
+                // Store results
+                ArrayList<Double> aeResults = new ArrayList<>();
+                ArrayList<Double> mseResults = new ArrayList<>();
+                // Test on each test set
+                for (ArrayList<ArrayList<String>> testSet : d.dataSets.testSets) {
+                    double absErr = 0;
+                    double meanErr = 0;
+                    // On each example in the test set
+                    for (ArrayList<String> test : testSet) {
+                        bestGA.initializeInputLayer(test);
+                        bestGA.feedForward();
+                        bestGA.calcErr();
+                        // Tally errors
+                        absErr += Math.abs(bestGA.error);
+                        meanErr += Math.pow(bestGA.error, 2);
                     }
-                    // Print Results
-                    System.out.println("Hidden Layers = " + finalI);
-                    printRangeAndMean(d.toString() + " data -     Absolute Error: ", aeResults);
-                    printRangeAndMean(d.toString() + " data - Mean Squared Error: ", mseResults);
-                });
+                    // Calculate results
+                    absErr /= testSet.size();
+                    meanErr /= testSet.size();
+                    aeResults.add(absErr);
+                    mseResults.add(meanErr);
+                }
+                // Print Results
+                System.out.println("Hidden Layers = " + i);
+                printRangeAndMean(d.toString() + " data -     Absolute Error: ", aeResults);
+                printRangeAndMean(d.toString() + " data - Mean Squared Error: ", mseResults);
             }
         }
-        executorService.shutdown();
-        try
 
         // run the tests for the three evolution strategies
         runParticleSwarmTests(rData, cData, numFailuresAllowed);
@@ -209,14 +167,12 @@ public class Driver {
         double sum = 0, min = Double.MAX_VALUE, max = Double.MIN_VALUE;
         for (double result: results)
         {
-            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+            sum += result;
+            if (result < min)
+                min = result;
+            if (result > max)
+                max = result;
         }
-        catch (InterruptedException e)
-        {
-            
-        }
-        for (Network goodOne: best)
-            System.out.println(goodOne);
         System.out.println(name + " from " + decimalFormat.format(min) + " to " + decimalFormat.format(max) +
                 " with a mean of " + decimalFormat.format(sum / results.size()) + ".");
     }
